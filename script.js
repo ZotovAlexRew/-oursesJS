65e82320ca6f178c2daa439d7b4c6e46d5b6bf31
zotovalexrew <che123zotov@gmail.com>
2022-01-27 12:58:29 +0300

    Выполнение первого урока

---
+++ script.js
@@ -0,0 +1,11 @@
+let title ="";
+let screens ="";
+let screenPrice ="";
+let rollback ="";
+let fullPrice ="";
+let adaptive ="";
+
+
+alert("Прохожу курсы от GloAcadevy");
+
+console.log("Меня зовут Саша");
