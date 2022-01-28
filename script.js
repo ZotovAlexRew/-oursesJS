917b2eccac4dd9be1fc597343ac3b01ebee0d520
zotovalexrew <che123zotov@gmail.com>
2022-01-28 09:37:43 +0300

    Выполнение второго урока, работа с выводом в консоль

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-let title ="";
-let screens ="";
-let screenPrice ="";
-let rollback ="";
-let fullPrice ="";
-let adaptive ="";
+let title = "JS-курсы";
+console.log(typeof title);
+
+let screens = "Простые, Сложные, Интерактивные";
+console.log(screens.length);
+console.log(screens.toLowerCase());
+console.log(screens.split(","));
 
 
-alert("Прохожу курсы от GloAcadevy");
+let screenPrice = 17;
+console.log("Стоимасть верстки экранов" + " " + screenPrice + " " + "долларов");
 
-console.log("Меня зовут Саша");
+let rollback = 99;
+console.log("Стоимасть разработки сайтов" + " " + rollback + " " + "долларов");
+
+
+let fullPrice = 10000;
+console.log(typeof fullPrice);
+
+
+let adaptive = true;
+console.log(typeof adaptive);
+
+
+
+console.log(fullPrice * (rollback / 100));
+
+//alert("Прохожу курсы от GloAcadevy");
+
+//console.log("Меня зовут Саша");
