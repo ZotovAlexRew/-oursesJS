7d0c1a88d95aa8fc52793d0e2da434bdd245b0bb
zotovalexrew <che123zotov@gmail.com>
2022-01-31 10:26:40 +0300

    Изменения не большие

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let title = "JS-курсы";
 let screens = "Простые, Сложные, Интерактивные";
+screens = screens.toLowerCase().split(",");
 let screenPrice = 17;
 let rollback = 99;
 let fullPrice = 10000;
@@ -8,12 +9,13 @@ let adaptive = true;
 
 console.log(typeof title);
 console.log(screens.length);
-console.log(screens.toLowerCase().split(","));
+console.log(screens);
 console.log("Стоимасть верстки экранов" + " " + screenPrice + " " + "долларов");
 console.log("Стоимасть разработки сайтов" + " " + rollback + " " + "долларов");
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(fullPrice * (rollback / 100));
 //alert("Прохожу курсы от GloAcadevy");
-
 //console.log("Меня зовут Саша");
+
+console.log(screens);
