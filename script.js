75707c370c14b55b40dc06a6c679943c71b33ecf
zotovalexrew <che123zotov@gmail.com>
2022-01-31 11:54:17 +0300

    Выполнение третьего урока

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-let title = "JS-курсы";
-let screens = "Простые, Сложные, Интерактивные";
-screens = screens.toLowerCase().split(",");
-let screenPrice = 17;
-let rollback = 99;
-let fullPrice = 10000;
-let adaptive = true;
+let title = prompt('Как называется ваш проект?');
+let screens = prompt('Какие типы экранов нужно разработать?');
+//screens = screens.toLowerCase().split(",");
+let screenPrice = prompt('Сколько будет стоить данная работа?');
+let rollback = 650;
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = prompt('Сколько это будет стоить?');
+let fullPrice =  Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
+let servicePercentPrice = Number(fullPrice) - Number(rollback/100);
 
 
-console.log(typeof title);
-console.log(screens.length);
+
+
+console.log(title);
+//console.log(screens.length);
 console.log(screens);
-console.log("Стоимасть верстки экранов" + " " + screenPrice + " " + "долларов");
-console.log("Стоимасть разработки сайтов" + " " + rollback + " " + "долларов");
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(fullPrice * (rollback / 100));
+console.log(screenPrice);
+//console.log("Стоимасть разработки сайтов" + " " + rollback + " " + "долларов");
+//console.log(typeof fullPrice);
+console.log(adaptive);
+//console.log(fullPrice * (rollback / 100));
 //alert("Прохожу курсы от GloAcadevy");
 //console.log("Меня зовут Саша");
+console.log(service1);
+console.log(servicePrice1);
+console.log(service2);
+console.log(servicePrice2);
+console.log(fullPrice);
+console.log(Math.ceil(servicePercentPrice));
 
-console.log(screens);
+
+if (fullPrice > 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice >= 15000 && fullPrice <= 30000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice == 0){
+  console.log("Скидки нет");
+} else if (fullPrice < 0) {
+  console.log("Упс, что-то пошло не так!");
+}
